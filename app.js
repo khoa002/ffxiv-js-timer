1a02ede6049f8fdc3ffb30637bd9d8acafb0fe6a
Khoa Nguyen <khoa002@gmail.com>
2016-08-30 15:41:18 -0700

    Changed redirect path to absolute

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/build'));
 app.get('/', function(req, res, n) {
-	res.sendFile('ffxiv_timer.html');
+	res.sendFile(__dirname + '/build/ffxiv_timer.html');
 });
 app.listen(port);
