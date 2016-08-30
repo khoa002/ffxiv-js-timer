461b987a8f72f44629e0ee7831efbe943d11e0c3
Khoa Nguyen <khoa002@gmail.com>
2016-08-30 15:38:40 -0700

    redirect root to build file

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(__dirname + '/build'));
+app.get('/', function(req, res, n) {
+	res.sendFile('ffxiv_timer.html');
+});
 app.listen(port);
