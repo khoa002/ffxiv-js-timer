a6d9fe785182ada327f70ae12bee729c694b47db
Khoa Nguyen <khoa002@gmail.com>
2016-08-30 17:36:01 -0700

    added paths to jquery and bootstrap from CDNs

--- web/ffxiv_main.js
+++ web/ffxiv_main.js
@@ -1,6 +1,8 @@
 requirejs.config({
 	paths: {
-		timer: '../timer'
+		'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min',
+		'bootstrap': '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min',
+		'timer': '../timer'
 	}
 });
 requirejs(['ffxiv_countdown'], function(FFXIVCountdown) {
